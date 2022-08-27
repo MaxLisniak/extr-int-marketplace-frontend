106ca5422abd5c1d2f6ad688ad2e9468200265c3
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-08-27 22:49:19 +0300

    sidebar changes

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import HomeIcon from "./assets/home.png";
 import CategoryIcon from "./assets/category.png";
 import StarIcon from "./assets/star.png";
+import AdminIcon from "./assets/admin.png";
 
 const Sidebar = () => {
 
@@ -13,6 +14,7 @@ const Sidebar = () => {
       </div>
       <div className="item"><img src={StarIcon} alt="Favorites" /></div>
       <div className="item"><img src={CategoryIcon} alt="Categories" /></div>
+      <div className="item"><Link to="/admin"><img src={AdminIcon} alt="Admin" /></Link></div>
     </div>
   )
 }
